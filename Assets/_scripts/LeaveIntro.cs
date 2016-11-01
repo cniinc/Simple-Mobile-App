56dde671ef12c0098e51d81670a55f23d1696187
Jason M <software@positrongames.com>
2016-10-31 18:31:26 -0700

    aaaand ratios didn't work. Time to fix that tomorrow!

---
+++ Assets/_scripts/LeaveIntro.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LeaveIntro : MonoBehaviour {
+	[SerializeField] float loadLength;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (LoadTransition ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator LoadTransition()
+	{
+		yield return new WaitForSeconds (loadLength);
+
+		SceneManager.LoadScene (1);
+	}
+}
